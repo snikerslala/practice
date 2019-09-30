77406bb718faa055ee8856f59bbe4b84afc94a22
Martyna Skup <46429607+snikerslala@users.noreply.github.com>
2019-09-30 11:38:44 +0200

    Updates

---
+++ precache-manifest.c351821823165d5cfdd2d0f91a48a50f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718a364893151e88cda48a49f7bdcd48",
+    "url": "/practice/index.html"
+  },
+  {
+    "revision": "9e51df5d7b9466c6393f",
+    "url": "/practice/static/css/main.e93bf6c0.chunk.css"
+  },
+  {
+    "revision": "1ef6764977a171102373",
+    "url": "/practice/static/js/2.6ec73b90.chunk.js"
+  },
+  {
+    "revision": "9e51df5d7b9466c6393f",
+    "url": "/practice/static/js/main.097a7d41.chunk.js"
+  },
+  {
+    "revision": "23f1d7065d46464aeee7",
+    "url": "/practice/static/js/runtime-main.a09bae5d.js"
+  }
+]);
