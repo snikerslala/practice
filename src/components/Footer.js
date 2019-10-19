d5ddb2d0ce5bcabbde60907af2937372cb334aa6
Martyna Skup <martyna.skup@yahoo.de>
2019-10-19 12:03:05 +0200

    Update footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer role="contentinfo" className="App-footer">
+    <footer role='contentinfo' className='App-footer'>
       <p>
         &copy; 2019 by Martyna Skup (
-        <a href="https://snikerslala.github.io/">snikerslala</a>)
+        <a href='https://snikerslala.github.io/'>snikerslala</a>)
       </p>
+      |
       <p>
-        <a href="https://github.com/snikerslala">GitHub</a>
+        <a href='https://github.com/snikerslala'>GitHub</a>
       </p>
+      |
       <p>
-        <a href="https://www.linkedin.com/in/martynaskup/">LinkedIn</a>
+        <a href='https://www.linkedin.com/in/martynaskup/'>LinkedIn</a>
       </p>
     </footer>
   );
