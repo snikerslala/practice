9bac6d7ecf07aa7111d9cdb15591c2ec06dc7707
Martyna Skup <martyna.skup@yahoo.de>
2019-10-30 22:12:33 +0100

    Add Styled Components for Header and Footer components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  background-color: #282c34;
+  color: white;
+  padding: 10px;
+  margin-bottom: 0;
+  font-size: 12px;
+
+  @media only screen and (min-width: 410px) {
+    font-size: 16px;
+  }
+`;
+const P = styled.p`
+  display: inline-block;
+  margin: 0 5px;
+
+  @media only screen and (min-width: 410px) {
+    padding: 10px;
+  }
+`;
+const PLine = styled.p`
+  display: inline-block;
+  margin: 0;
+  padding: 10px 0;
+`;
+const A = styled.a`
+  color: white;
+  padding: 2px;
+`;
 
 const Footer = () => {
   return (
-    <footer role='contentinfo' className='App-footer'>
-      <p>
+    <StyledFooter role='contentinfo' className='App-StyledFooter'>
+      <P>
         &copy; 2019 by Martyna Skup (
-        <a href='https://snikerslala.github.io/'>snikerslala</a>)
-      </p>
-      |
-      <p>
-        <a href='https://github.com/snikerslala'>GitHub</a>
-      </p>
-      |
-      <p>
-        <a href='https://www.linkedin.com/in/martynaskup/'>LinkedIn</a>
-      </p>
-    </footer>
+        <A href='https://snikerslala.github.io/'>snikerslala</A>)
+      </P>
+      <PLine>|</PLine>
+      <P>
+        <A href='https://github.com/snikerslala'>GitHub</A>
+      </P>
+      <PLine>|</PLine>
+      <P>
+        <A href='https://www.linkedin.com/in/martynaskup/'>LinkedIn</A>
+      </P>
+    </StyledFooter>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeaderDiv = styled.div`
+  background-color: #282c34;
+  padding: 20px 20px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+const Nav = styled.nav`
+  font-size: calc(10px + 1vmin);
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Ul = styled.ul`
+  padding: 0;
+`;
+const Li = styled.li`
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid burlywood;
+  display: inline;
+  list-style: none;
+`;
 
 const Header = ({ changeContent }) => {
   return (
-    <div className="App-header">
-      <header role="banner">Practice examples for React</header>
-      <nav role="navigation">
-        <p name="form" onClick={name => changeContent('form')}>
-          Form
-        </p>
-        <p name="todo" onClick={name => changeContent('todo')}>
-          Todo List
-        </p>
-      </nav>
-    </div>
+    <StyledHeaderDiv className='App-header'>
+      <header role='banner'>Practice examples for React</header>
+      <Nav role='navigation'>
+        <Ul>
+          <Li name='form' onClick={name => changeContent('form')}>
+            Form
+          </Li>
+          <Li name='todo' onClick={name => changeContent('todo')}>
+            Todo List
+          </Li>
+          <Li name='balls' onClick={name => changeContent('balls')}>
+            Balls
+          </Li>
+        </Ul>
+      </Nav>
+    </StyledHeaderDiv>
   );
 };
 
