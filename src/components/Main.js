f38ad81e31534472a0fa7189a9b1c5f5a1171122
Martyna Skup <martyna.skup@yahoo.de>
2019-10-31 07:59:23 +0100

    Add Balls component to the Main page route in Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import TodoItemList from './TodoList/TodoItemList';
 import Form from './Form/Form';
+import Balls from './Balls/Balls';
 
 const Main = ({ content }) => {
   return (
-    <main role="main" className="App-main">
-      {content === 'todo' ? <TodoItemList /> : <Form />}
+    <main role='main' className='App-main'>
+      {content === 'todo' ? (
+        <TodoItemList />
+      ) : content === 'form' ? (
+        <Form />
+      ) : (
+        <Balls />
+      )}
     </main>
   );
 };
