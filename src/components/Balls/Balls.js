a7d956a15e433b6a62df52e89ebf90ca388b3c03
Martyna Skup <martyna.skup@yahoo.de>
2019-11-01 17:43:42 +0100

    Add Balls component
    1. Add Balls folder
    2. Add Balls component with one sample ball
    3. Add styling to Balls component with infinite movement animation based on keyframes

---
+++ src/components/Balls/Balls.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Balls.css';
+
+const Balls = () => {
+  console.log('hello');
+
+  return (
+    <div>
+      <div className='ball jump'></div>
+      <p>I jump!</p>
+    </div>
+  );
+};
+
+export default Balls;
